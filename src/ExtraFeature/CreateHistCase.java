022af2384c4ac42ff694af325ac148d86952b35b
ninjadororo25@gmail.com <ninjadororo25@gmail.com>
2022-06-22 23:23:14 +0800

    update CreateHistCase.java

--- src/ExtraFeature/CreateHistCase.java
+++ src/ExtraFeature/CreateHistCase.java
@@ -2,6 +2,8 @@ package ExtraFeature;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -14,11 +16,11 @@ public class CreateHistCase {
         filename = filename.trim();
 
         Random random = new Random();
-        int numberdata = random.nextInt(1000);
-        int numberbin = random.nextInt(20);
+        int numberdata = random.nextInt(100000);
+        int numberbin = random.nextInt(40);
         PrintWriter writer=null;
         try {
-            File Fileright = new File("C:\\Users\\User\\Downloads\\New Down\\HistBench\\" + filename + ".txt");
+            File Fileright = new File("HistCase\\" + filename + ".txt");
             boolean test = Fileright.createNewFile();
             if (test) {
                 System.out.println("File created" + Fileright.getCanonicalPath());
@@ -48,63 +50,46 @@ public class CreateHistCase {
 
     public static String Ans(int numberdata, int numberbin, String dataset){
 
-        int no_of_data = numberdata;
-        int no_of_bins = numberbin;
+        int nodata = numberdata;
+        int nobins = numberbin;
 
-        int[] datapoints = new int[no_of_data];
-
-        int b = 0;
+        Integer[] datapoints = new Integer[nodata];
         String[] split = dataset.split(" ");
-        while (b < no_of_data) {
+        for(int b = 0;b<nodata;b++) {
             datapoints[b] = Integer.parseInt(split[b]);
-            b++;
         }
 
-        int maximum = datapoints[0];
-        int minimum = datapoints[0];
+        int max = Collections.max(Arrays.asList(datapoints));
+        int min = Collections.min(Arrays.asList(datapoints));
 
-        for (int i = 0; i < datapoints.length; i++) {
+        int cutoffs = (max - min) / nobins;
+        int[] interval = new int[nobins + 1];
+        int[] count = new int[nobins];
 
-            if (datapoints[i] > maximum) {
-                maximum = datapoints[i];
-            } else if (datapoints[i] < minimum) {
-                minimum = datapoints[i];
-            }
+        for (int i = 0; i < interval.length; i++) {
+            interval[i] = min;
+            min += cutoffs;
         }
 
-        int cutoffs_value = (maximum - minimum) / no_of_bins;
-        int sum = 0;
-        int[] interval = new int[no_of_bins + 1];
-        int[] count = new int[no_of_bins];
-        String line = "";
-
-        for (int j = 0; j < interval.length; j++) {
-            if (j == 0) {
-                sum += minimum;
-                interval[j] = minimum;
-                line += minimum + " ";
-            } else {
-                interval[j] = sum;
-                line += sum + " ";
-            }
-            sum += cutoffs_value;
-        }
-
-        line += "\n";
-
-        for (int k = 0; k < datapoints.length; k++) {
-            for (int l = 0; l < interval.length - 1; l++) {
-                if (datapoints[k] >= interval[l] && datapoints[k] < interval[l + 1]) {
-                    count[l]++;
+        Loop:
+        for (int data : datapoints) {
+            for (int k = 0; k < interval.length - 2; k++) {
+                if (data < interval[k + 1]) {
+                    count[k]++;
+                    continue Loop;
                 }
             }
-            if (datapoints[k] == interval[interval.length - 1]) {
-                count[interval.length - 2]++;
-            }
+            count[nobins-1]++;
         }
 
-        for (int m = 0; m < count.length; m++) {
-            line += count[m] + " ";
+        String line="";
+        for (int cutoff: interval) {
+            line += cutoff + " ";
+        }
+
+        line+="\n";
+        for (int nocount: count) {
+            line += nocount + " ";
         }
         return line;
     }
